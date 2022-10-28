d8238e453c80121302fb0b310846de824ace68ba
Lisa-Ann B <lisabruney@pitt.edu>
2022-10-28 08:46:16 -0400

    Add login functionality

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,17 +7,18 @@ describe('portal ui login', () => {
     cy.visit(PATHS.search)
     cy.contains('Sign-in').click()
     cy.contains('Log in with your institution credentials').click()
-    cy.origin('auth.globus.org', () => {
-      cy.contains('Continue').click()
-      cy.contains('Continue').click()
-      cy.get('#google_signin_btn').click()
+    cy.origin('auth.globus.org',  { args: { AUTH } }, ({ AUTH }) => {
+      // Use Globus
+      cy.contains('Globus ID to sign in').click()
     })
 
-    cy.origin('accounts.google.com', () => {
-      cy.get('input').type(AUTH.user)
-      cy.contains('Next').click()
+    cy.origin('www.globusid.org', { args: { AUTH } }, ({ AUTH })  => {
+      cy.get('input[name="username"]').type(AUTH.user)
+      cy.get('input[name="password"]').type(AUTH.password)
+      cy.get('button[type="submit"]').click()
     })
-    // cy.visit('http://localhost:3000/search')
-    // cy.contains('Sign-out')
+
+    cy.contains('Sign-out')
+
   })
 })
